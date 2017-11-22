7bb84869c2a00e76e514e75279aed6ae4daacf9f
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2017-11-22 16:17:30 +0100

    restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0149887564",
+    category:     "chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "0149547564",
+    category:     "italian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "0197887564",
+    category:     "french"
+  },
+  {
+    name:         "Woko",
+    address:      "confluence Lyon",
+    phone_number: "0469684564",
+    category:     "japanese"
+  },
+  {
+    name:         "Tire bouchon",
+    address:      "Vieux lyon, Lyon",
+    phone_number: "0692835477",
+    category:     "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
