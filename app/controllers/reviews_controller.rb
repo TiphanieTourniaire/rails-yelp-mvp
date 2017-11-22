27c770fa50264d58269b04d79050570717d09905
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2017-11-22 17:02:03 +0100

    Restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
 
+before_action :set_restaurant, only: [:new, :create]
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
@@ -9,15 +10,25 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
     if @review.save
-    redirect_to restaurant_path(@review.restaurant)
-  else
-    render :new
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
   end
-  end
+
+  # def display
+  #   @reviews = Review.where(restaurand_id: @restaurant.id)
+  # end
+
 
   private
 
   def review_params
     params.require(:review).permit(:rating, :content)
   end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
 end
